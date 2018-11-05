751c2588b2c526a4e590ed00ce5780a6f7bac904
WoodYuang <38492941+WoodYuang@users.noreply.github.com>
2018-11-05 13:14:56 +0800

    Realizing rotation function

---
+++ newport_test/newport_test/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace newport_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "串口连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "断开连接";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(260, 181);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 36);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "波片归零";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "串口";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(292, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(108, 21);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "串口状态";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "波特率";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(85, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(97, 20);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(83, 70);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(103, 20);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(63, 154);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 38);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "转动角度";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(55, 21);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "波片角度";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(53, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "波片角度";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(112, 223);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(55, 21);
+            this.textBox3.TabIndex = 15;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(63, 263);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(133, 35);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "继续转动";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(311, 244);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(75, 21);
+            this.textBox4.TabIndex = 17;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(218, 247);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 12);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "当前波片角度";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(232, 286);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "当前波片";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(311, 283);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(75, 21);
+            this.textBox5.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 341);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.IO.Ports.SerialPort myserialPort;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ newport_test/newport_test/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.IO.Ports;
+
+namespace newport_test
+{
+    public partial class Form1 : Form
+    {
+        //串口通信声明
+        string returnflag;
+        bool tryagain; //防止串口不稳定出错
+
+        //波片位置
+        int rotnum;
+
+
+        //波片架0度位置
+        double oriposition1=0;
+        double toposition;
+        double addposition;
+        double nowposition;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 串口连接
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (myserialPort.IsOpen == true)
+                return;
+            try
+            {
+                myserialPort.PortName = comboBox1.Text;
+                myserialPort.BaudRate = Convert.ToInt32(comboBox2.Text.Trim());//"57600"
+                myserialPort.Parity = Parity.None;
+                myserialPort.DataBits = 8;
+                myserialPort.StopBits = StopBits.One;
+
+                myserialPort.Open();
+            }
+            catch (Exception exception)
+            {
+                System.Windows.Forms.MessageBox.Show(exception.Message);
+            }
+            finally
+            {
+                if (myserialPort.IsOpen == true)
+                {
+                    textBox2.Text = "串口已连接";
+                    textBox2.ForeColor = Color.Green;
+                }
+                else
+                {
+                    textBox2.Text = "串口未连接";
+                    textBox2.ForeColor = Color.Red;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //初始化串口参数
+            string[] myports = SerialPort.GetPortNames();
+            for (int i = 0; i <= myports.Length - 1; i++)
+            {
+                string myport = myports[i];
+                comboBox1.Items.Add(myport.Trim());
+            }
+            comboBox1.SelectedIndex = 0;
+            comboBox2.Items.Add("57600");
+            comboBox2.SelectedIndex = comboBox2.Items.IndexOf("57600");
+            myserialPort = new SerialPort();
+
+            //初始化SerialPort对象  
+            myserialPort.NewLine = "/r/n";
+            myserialPort.RtsEnable = true; //放着先看看
+
+            //波片初始化
+            textBox5.Text = "1";  //默认操作第一个波片
+
+            //添加事件注册  
+            myserialPort.DataReceived += myserialPort_DataReceived;
+        }
+
+        /// <summary>
+        /// 串口数据采集
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void myserialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int spn = myserialPort.BytesToRead;//先记录下来，避免某种原因，人为的原因，操作几次之间时间长，缓存不一致  
+            byte[] mybyte = new byte[spn];
+            string returnflaghere;
+            try
+            {
+                if (spn > 0)
+                {
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        System.Windows.Forms.Application.DoEvents();
+                    }
+                    myserialPort.Read(mybyte, 0, spn);
+                    myserialPort.DiscardInBuffer();
+                    returnflaghere = "";
+                    foreach (byte inbyte in mybyte)
+                    {
+                        returnflaghere += Convert.ToChar(inbyte);
+                    }
+                    returnflag += returnflaghere;
+                }
+            }
+            catch (Exception exception)
+            {
+                System.Windows.Forms.MessageBox.Show(exception.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// 串口断开
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (myserialPort.IsOpen == false)
+                return;
+
+            try
+            {
+                myserialPort.Close();
+            }
+            catch (Exception exception)
+            {
+                System.Windows.Forms.MessageBox.Show(exception.Message);
+            }
+            finally
+            {
+                if (myserialPort.IsOpen == true)
+                {
+                    textBox2.Text = "串口已连接";
+                    textBox2.ForeColor = Color.Green;
+                }
+                else
+                {
+                    textBox2.Text = "串口未连接";
+                    textBox2.ForeColor = Color.Red;
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            rotnum =Convert.ToInt32(textBox5.Text.Trim());
+            nowposition=rotate(oriposition1, rotnum);
+            textBox4.Text =nowposition.ToString();
+            System.Windows.Forms.MessageBox.Show("波片已归零！", "消息");
+        }
+
+
+
+        /// <summary>
+        /// 转动波片架
+        /// </summary>
+        /// <param name="position"></param>//要转动的角度
+        /// <param name="rotnumx"></param>//要转动哪个波片架
+        private double rotate(double position, int rotnumx)
+        {
+            int rotnum = rotnumx;
+            double myposition;
+            tryagain = false;
+
+            if (operation(rotnum, rotnum.ToString() + "PA" + position.ToString(), false) == "")
+            {
+                return -1;
+            }
+
+            do
+            {
+                for (int i = 0; i <= 1000000; i++)
+                {
+                    System.Windows.Forms.Application.DoEvents();
+                }
+
+                string retval = operation(rotnum, rotnum.ToString() + "TP", true);
+                if (tryagain == true)
+                {
+                    retval = operation(rotnum, rotnum.ToString() + "TP", true);
+                }
+                if (tryagain == true)
+                {
+                    retval = operation(rotnum, rotnum.ToString() + "TP", true);
+                }
+
+
+                if (retval == "")
+                    return -1;
+                  myposition = Convert.ToDouble(retval.Substring((rotnum.ToString() + "TP").Length));
+                if (Math.Abs(myposition - position) < 1)
+                    break;
+            } while (true);
+            return myposition;
+        }
+        private string operation(int myrotnum, string mycmd, bool rtnneeded)
+        {
+            try
+            {
+                returnflag = "";
+                myserialPort.Write(mycmd.Trim() + "\r" + "\n");
+            }
+            catch (Exception exception)
+            {
+                System.Windows.Forms.MessageBox.Show(exception.Message);
+            }
+
+            if (rtnneeded == false)
+            {
+                return "done";
+            }
+
+            int pp = 0;
+            do
+            {
+                for (int i = 0; i <= 10000; i++)
+                {
+                    System.Windows.Forms.Application.DoEvents();
+                }
+                if (returnflag.Length >= 2)
+                {
+                    if (returnflag.Substring(returnflag.Length - 2) == "\r" + "\n")
+                    {
+                        tryagain = false;
+                        break;
+                    }
+                }
+
+                pp++; //防止串口出错，转波片等待10余秒还没结果，自动跳出，再来一次
+                if (pp > 2000)
+                {
+                    tryagain = true;
+                    break;
+                }
+
+            } while (true);
+            return returnflag.Trim();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+//转动一次
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+                toposition = Convert.ToInt32(textBox1.Text.Trim());
+                rotnum = Convert.ToInt32(textBox5.Text.Trim());
+                nowposition =rotate(oriposition1+toposition, rotnum);
+                textBox4.Text = nowposition.ToString();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+//持续转动
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text != "")
+                addposition = Convert.ToInt32(textBox3.Text.Trim());
+                rotnum = Convert.ToInt32(textBox5.Text.Trim());
+                nowposition =rotate(nowposition + addposition, rotnum);
+                textBox4.Text = nowposition.ToString();
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+  }
